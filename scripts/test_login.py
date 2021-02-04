109ee628a79a7009d04ff178fad6c38fa4fde685
lljoshua <514411957@qq.com>
2021-02-04 17:41:56 +0800

    jenkin_git

---
+++ scripts/test_login.py
@@ -0,0 +1,18 @@
+class TestLogin():
+    def test_hello1(self):
+        assert True
+
+    def test_hello2(self):
+        assert  False
+
+    def test_hello3(self):
+        assert False
+
+    def test_hello4(self):
+        assert  False
+
+    def test_hello5(self):
+        assert True
+
+    def test_hello6(self):
+        assert True
